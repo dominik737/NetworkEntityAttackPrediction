2689db07a64ede70cd21424e39b749990fbbf997
Dominik <dominik.zacek@pinya.cz>
2022-05-29 18:12:18 +0200

    publish fix

--- Publisher/Publisher.py
+++ Publisher/Publisher.py
@@ -6,9 +6,13 @@ from Database.Models.EntityModel import EntityModel
 
 def publish(db: Database, args):
     entities: List[EntityModel] = db.session.query(EntityModel).filter(EntityModel.FMP_score > 0).all()
-    low_entities: List[EntityModel] = list(filter(lambda x: x.FMP_score > float(args.publish[0]), entities))
-    medium_entities: List[EntityModel] = list(filter(lambda x: x.FMP_score > float(args.publish[1]), entities))
-    high_entities: List[EntityModel] = list(filter(lambda x: x.FMP_score > float(args.publish[2]), entities))
+    low_threshold = float(args.publish[0])
+    medium_threshold = float(args.publish[1])
+    high_threshold = float(args.publish[2])
+
+    low_entities: List[EntityModel] = list(filter(lambda x: low_threshold <= x.FMP_score < medium_threshold, entities))
+    medium_entities: List[EntityModel] = list(filter(lambda x: medium_threshold <= x.FMP_score < high_threshold, entities))
+    high_entities: List[EntityModel] = list(filter(lambda x: high_threshold <= x.FMP_score, entities))
 
     make_publish_file(low_entities, "low.txt")
     make_publish_file(medium_entities, "medium.txt")
